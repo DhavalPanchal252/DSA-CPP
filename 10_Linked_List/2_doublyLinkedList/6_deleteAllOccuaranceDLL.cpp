cc66cf81c1d06b242eeb48f2bfb7b72897e24c49
Dhaval <panchaldhaval797@gmail.com>
2026-02-11 12:49:08 +0530

    delete all occurance of nodes

---
+++ 10_Linked_List/2_doublyLinkedList/6_deleteAllOccuaranceDLL.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int data1 , Node* prev1 , Node* next1){
+        data = data1;
+        prev = prev1;
+        next = next1;
+    }
+
+    Node(int data1){
+        data = data1;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+Node* convertArrToDLL(vector<int> &arr , int n){
+
+    if(n == 0) return nullptr;
+
+    Node* head = new Node(arr[0]);
+    Node* back = head;
+    for(int i=1 ; i<n ; i++){
+        Node* temp = new Node(arr[i] , back , nullptr);
+        back->next = temp;
+        back = back->next;
+    }
+    return head;
+}
+
+Node* optimal(Node* head , int x){
+
+    Node* temp = head;
+
+    while(temp){
+        if(temp->data == x){
+            Node* del = temp;
+            Node* nextNode = temp->next;
+
+            if(del == head){
+                head = nextNode;
+
+                if(head) head->prev = nullptr;
+                
+            }
+            else{
+
+                if(del->prev) del->prev->next = nextNode;
+                if(del->next) nextNode->prev = del->prev;
+                 
+            }
+            delete del; 
+            temp = nextNode;
+        }
+        else{
+            temp = temp->next;
+        }
+    }
+    return head;
+}
+
+int main(){
+    
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(auto &i : arr) cin >> i;
+
+    int ele;
+    cin >> ele;
+
+
+    Node* node = convertArrToDLL(arr,n);
+
+    Node* head = optimal(node,ele);
+
+    while(head){
+        cout << head->data << " ";
+        head = head->next;
+    }    
+    return 0;
+}
